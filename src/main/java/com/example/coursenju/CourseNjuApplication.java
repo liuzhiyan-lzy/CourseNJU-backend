9abafe730280e767e1ae0137cc8ade555b0b0e2d
liuzhiyan-lzy <lzyskyfall@qq.com>
2022-03-05 16:15:56 +0800

    backend

---
+++ src/main/java/com/example/coursenju/CourseNjuApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursenju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseNjuApplication.class, args);
+    }
+
+}
